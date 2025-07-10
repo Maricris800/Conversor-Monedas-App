37f21477be2d4d0a0265b22ec9a12325ef810237
Maricris800 <maricris80060@gmail.com>
2025-07-09 21:25:01 -0400

    mas cambios

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://maricris800.github.io/Conversor-Monedas-App/"
 })
